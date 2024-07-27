d274d625a52f0dcc1260414cf4e8f7d8bf9ed870
jnfrdm <115798337+jnfrdm@users.noreply.github.com>
2024-07-27 21:47:56 +0200

    Update Understand if a MIDI file has multiple BPM.py

--- Understand if a MIDI file has multiple BPM.py
+++ Understand if a MIDI file has multiple BPM.py
@@ -1,9 +1,14 @@
+# This is a python script
+# You can run it by :
+# 1) Typing "python Understand if a MIDI file has multiple BPM.py" on the terminal. Be sure that the script is present in the currect directory!
+# 2) Using and IDE like Visual Studio code or PyCharm
+
 import mido
 from mido import MidiFile
 import os
 
 # Input paths
-midi_path = ""
+midi_path = "" # <- Put here the path of the midi song you want to know if is multiple BPM
 
 # Load Midi file
 mid = MidiFile(midi_path)
